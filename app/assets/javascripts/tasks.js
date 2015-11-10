02307100cdec13f13ed14b37905c71853d5f7a3b
Damon Davison <damon@curatur.com>
2015-11-10 14:35:58 -0500

    Rough draft of working AJAX

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,14 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+//
+function ready(){
+  console.log("document ready");
+  $('#new_task').on("ajax:success", function(evt, data) {
+    //window.location.reload();
+    $('body').append(data);
+    $('#task_name').val("");
+  }).on("ajax:error", function() { alert("Oops!"); });
+}
+
+$(document).on('ready page:load', ready);
+
